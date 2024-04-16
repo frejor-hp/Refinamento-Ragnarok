8d903f4f4d7ed13332ca2546f89ef873b809c3fc
frejor-hp <lipe.oli.mo@hotmail.com>
2024-04-16 17:28:15 -0300

    código mais limpo, referencia a mediana e moda

--- script.js
+++ script.js
@@ -1,52 +1,94 @@
-var num = 11;
+var refinoInicial = 12;
+var refinoFinal = 15;
+var refinoAtual = refinoInicial;
 var bsb = 0;
-var tentativas;
+var tentativas = 120000;
 var bsbTentativas = [];
-var soma = 0;
 var media;
+var mediana;
+var moda;
 
-for(tentativas = 100000; tentativas > 0; tentativas--){
-    while(num < 14){
-        if(num == 11){
-            if(Math.random() <= 0.18){
-                num++;
-            }
-            bsb++;
-        }
+for(; tentativas > 0; tentativas--){
 
-        if(num == 12){
-            if(Math.random() <= 0.15){
-                num++;
+    while(refinoAtual < refinoFinal){
+
+        if(refinoAtual == 15){
+            if(Math.random() <= 0.14){
+                refinoAtual++;
             } else{
-                num--;
+                refinoAtual--;
             }
         }
 
-        if(num == 13){
+        if(refinoAtual == 14){
+            if(Math.random() <= 0.14){
+                refinoAtual++;
+            } else{
+                refinoAtual--;
+            }
+        }
+
+        if(refinoAtual == 13){
             if(Math.random() <= 0.15){
-                num++;
+                refinoAtual++;
             }
             bsb += 22;
         }
 
-        /*if(num == 14){
-            if(Math.random() <= 0.14){
-                num++;
+        if(refinoAtual == 12){
+            if(Math.random() <= 0.15){
+                refinoAtual++;
             } else{
-                num--;
+                refinoAtual--;
             }
-        }*/
+        }
+
+        if(refinoAtual == 11){
+            if(Math.random() <= 0.18){
+                refinoAtual++;
+            }
+            bsb++;
+        }
     }
+
     bsbTentativas.push(bsb);
-    num = 11;
+    refinoAtual = refinoInicial;
     bsb = 0;
 }
 
-for(var i = bsbTentativas.length; i > 0; i--){
-    soma += bsbTentativas[i-1];
-}
-media = soma / bsbTentativas.length;
+bsbTentativas.sort(compareNumbers);
 
-console.log("gasto em bsb por tentativa: ", bsbTentativas);
-console.log("gasto médio: ", media);
-console.log("maior gasto: ", Math.max(...bsbTentativas));
+media = mediaArray(bsbTentativas);
+
+mediana = medianaArray(bsbTentativas);
+
+moda = modaArray(bsbTentativas);
+
+console.log("Média: ", media);
+console.log("Mediana: ", mediana);
+console.log("Moda: ", moda);
+console.log("Maior Gasto: ", Math.max(...bsbTentativas));
+
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+function mediaArray(valores){
+    var soma = 0;
+    for(i in valores){
+        soma += valores[i];
+    }
+    return soma/valores.length;
+}
+
+function medianaArray(valores){
+    return valores[Math.floor(valores.length/2)];
+}
+
+function modaArray(valores){
+    var repeticoes = new Array(valores.length).fill(0)
+    for(i in valores){
+        repeticoes[valores[i]]++;
+    }
+    return repeticoes.indexOf(Math.max(...repeticoes))
+}
